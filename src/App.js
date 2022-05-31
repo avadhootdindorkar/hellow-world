6eeb87023e3a0fb42cffcb623d13ffb7a9957ec5
Avadhoot Dindorkar <adindorkar@affirma.com>
2022-05-31 17:19:21 +0530

    Render props demo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import Form from './components/Form';
 import ClickCounter from './components/ClickCounter';
 import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import RenderPropsCounter from './components/RenderPropsCounter';
 
 function App() {
   return (
     <div className="App">
-      <ClickCounter name = 'Avadhoot'/>
-      <HoverCounter name = 'Bhavana'/>
+      <RenderPropsCounter render={(count, incrementCount) => (
+        <ClickCounterTwo count={count} incrementCount={incrementCount} />
+      )} />
+
+      <RenderPropsCounter render={(count, incrementCount) => (
+        <HoverCounterTwo count={count} incrementCount={incrementCount} />
+      )} />
+
+      {/* <ClickCounterTwo />
+      <HoverCounterTwo /> */}
       {/* <Form/> */}
     </div>
   );
